30a26c176225f9e4221d91e6a03c3b26d0023faa
Julián D. González V <judgonzalezv@correo.udistrital.edu.co>
2021-09-06 13:02:52 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,256 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
+from passlib.hash import sha256_crypt
+from functools import wraps
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+import timeit
+import datetime
+from flask_mail import Mail, Message
+import os
+from wtforms.fields.html5 import EmailField
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/image/product'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+
+# Config MySQL
+mysql = MySQL()
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'nur'
+app.config['MYSQL_PASSWORD'] = 'secret'
+app.config['MYSQL_DB'] = 'menshut'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialize the app for use with this MySQL class
+mysql.init_app(app)
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, *kwargs)
+        else:
+            return redirect(url_for('login'))
+
+    return wrap
+
+
+def not_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return redirect(url_for('index'))
+        else:
+            return f(*args, *kwargs)
+
+    return wrap
+
+
+def is_admin_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin_logged_in' in session:
+            return f(*args, *kwargs)
+        else:
+            return redirect(url_for('admin_login'))
+
+    return wrap
+
+
+def not_admin_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin_logged_in' in session:
+            return redirect(url_for('admin'))
+        else:
+            return f(*args, *kwargs)
+
+    return wrap
+
+
+def wrappers(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+
+    return wrapped
+
+
+def content_based_filtering(product_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM products WHERE id=%s",
+                (product_id,))  # getting id row
+    data = cur.fetchone()  # get row info
+    data_cat = data['category']  # get id category ex shirt
+    print('Showing result for Product Id: ' + product_id)
+    category_matched = cur.execute(
+        "SELECT * FROM products WHERE category=%s", (data_cat,))  # get all shirt category
+    print('Total product matched: ' + str(category_matched))
+    cat_product = cur.fetchall()  # get all row
+    cur.execute("SELECT * FROM product_level WHERE product_id=%s",
+                (product_id,))  # id level info
+    id_level = cur.fetchone()
+    recommend_id = []
+    cate_level = ['v_shape', 'polo', 'clean_text', 'design', 'leather', 'color', 'formal', 'converse', 'loafer', 'hook',
+                  'chain']
+    for product_f in cat_product:
+        cur.execute(
+            "SELECT * FROM product_level WHERE product_id=%s", (product_f['id'],))
+        f_level = cur.fetchone()
+        match_score = 0
+        if f_level['product_id'] != int(product_id):
+            for cat_level in cate_level:
+                if f_level[cat_level] == id_level[cat_level]:
+                    match_score += 1
+            if match_score == 11:
+                recommend_id.append(f_level['product_id'])
+    print('Total recommendation found: ' + str(recommend_id))
+    if recommend_id:
+        cur = mysql.connection.cursor()
+        placeholders = ','.join((str(n) for n in recommend_id))
+        query = 'SELECT * FROM products WHERE id IN (%s)' % placeholders
+        cur.execute(query)
+        recommend_list = cur.fetchall()
+        return recommend_list, recommend_id, category_matched, product_id
+    else:
+        return ''
+
+
+@app.route('/')
+def index():
+    form = OrderForm(request.form)
+    # Create cursor
+    cur = mysql.connection.cursor()
+    # Get message
+    values = 'tshirt'
+    cur.execute(
+        "SELECT * FROM products WHERE category=%s ORDER BY RAND() LIMIT 4", (values,))
+    tshirt = cur.fetchall()
+    values = 'wallet'
+    cur.execute(
+        "SELECT * FROM products WHERE category=%s ORDER BY RAND() LIMIT 4", (values,))
+    wallet = cur.fetchall()
+    values = 'belt'
+    cur.execute(
+        "SELECT * FROM products WHERE category=%s ORDER BY RAND() LIMIT 4", (values,))
+    belt = cur.fetchall()
+    values = 'shoes'
+    cur.execute(
+        "SELECT * FROM products WHERE category=%s ORDER BY RAND() LIMIT 4", (values,))
+    shoes = cur.fetchall()
+    # Close Connection
+    cur.close()
+    return render_template('home.html', tshirt=tshirt, wallet=wallet, belt=belt, shoes=shoes, form=form)
+
+
+class LoginForm(Form):  # Create Login Form
+    username = StringField('', [validators.length(min=1)],
+                           render_kw={'autofocus': True, 'placeholder': 'Username'})
+    password = PasswordField('', [validators.length(min=3)],
+                             render_kw={'placeholder': 'Password'})
+
+
+# User Login
+@app.route('/login', methods=['GET', 'POST'])
+@not_logged_in
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        # GEt user form
+        username = form.username.data
+        # password_candidate = request.form['password']
+        password_candidate = form.password.data
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute(
+            "SELECT * FROM users WHERE username=%s", [username])
+
+        if result > 0:
+            # Get stored value
+            data = cur.fetchone()
+            password = data['password']
+            uid = data['id']
+            name = data['name']
+
+            # Compare password
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                session['logged_in'] = True
+                session['uid'] = uid
+                session['s_name'] = name
+                x = '1'
+                cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
+
+                return redirect(url_for('index'))
+
+            else:
+                flash('Incorrect password', 'danger')
+                return render_template('login.html', form=form)
+
+        else:
+            flash('Username not found', 'danger')
+            # Close connection
+            cur.close()
+            return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route('/out')
+def logout():
+    if 'uid' in session:
+        # Create cursor
+        cur = mysql.connection.cursor()
+        uid = session['uid']
+        x = '0'
+        cur.execute("UPDATE users SET online=%s WHERE id=%s", (x, uid))
+        session.clear()
+        flash('You are logged out', 'success')
+        return redirect(url_for('index'))
+    return redirect(url_for('login'))
+
+
+class RegisterForm(Form):
+    name = StringField('', [validators.length(min=3, max=50)],
+                       render_kw={'autofocus': True, 'placeholder': 'Full Name'})
+    username = StringField('', [validators.length(min=3, max=25)], render_kw={
+                           'placeholder': 'Username'})
+    email = EmailField('', [validators.DataRequired(), validators.Email(), validators.length(min=4, max=25)],
+                       render_kw={'placeholder': 'Email'})
+    password = PasswordField('', [validators.length(min=3)],
+                             render_kw={'placeholder': 'Password'})
+    mobile = StringField('', [validators.length(min=11, max=15)], render_kw={
+                         'placeholder': 'Mobile'})
+
+
+@app.route('/register', methods=['GET', 'POST'])
+@not_logged_in
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        mobile = form.mobile.data
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email, username, password, mobile) VALUES(%s, %s, %s, %s, %s)",
+                    (name, email, username, password, mobile))
+
+        # Commit cursor
+        mysql.connection.commit()
+
+        # Close Connection
+        cur.close()
+
+        flash('You are now registered and can login', 'success')
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
